5c12c8cf16789085ab9c8e0545f4ecaabfe1853a
Cauã Silva <csilvanp@gmail.com>
2021-03-04 20:07:50 -0300

    setting grid

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 250px auto;
+  grid-template-rows: 70px auto;
+  grid-template-areas:
+  // AS = Aside
+  // MH = Main Header
+  // CT = Content
+    'AS MH'
+    'AS CT';
+  height: 100vh;
+`;
