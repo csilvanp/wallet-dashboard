e1bae8e67abcf1a9c067dd15ca921a3830525ce7
Cauã Silva <csilvanp@gmail.com>
2021-03-08 16:02:17 -0300

    added themes & colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Layout from './components/Layout';
+import dark from './styles/themes/dark';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  title: 'light',
+  colors: {
+    primary: '#DCDCDC',
+    secondary: '#FFF',
+    tertiary: '#F5F5F5',
+
+    white: '#000',
+    black: '#FFF',
+    gray: '#BFBFBF',
+
+    success: '#03BB85',
+    info: '#3B5998',
+    warning: '#FF6961',
+  }
+};
