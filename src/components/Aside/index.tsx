f8ef938cf681e884390d8b8d0b0515690630f924
Cauã Silva <csilvanp@gmail.com>
2021-03-08 20:57:46 -0300

    added aside menu & react-icons

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import { Container } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Container, Header, LogoImg, Title, MenuContainer, MenuItemLink } from './styles';
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp
+} from 'react-icons/md';
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={logoImg} alt="Logo Wallet" />
+        <Title>Wallet</Title>
+      </Header>
+
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   )
 }
