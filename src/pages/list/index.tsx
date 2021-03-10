14f9c23f4a853ff72b8786825b32ff5cb3302b29
Cauã Silva <csilvanp@gmail.com>
2021-03-10 19:32:17 -0300

    adding list filters

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -3,18 +3,48 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
-  const options = [
-    { value: 'caua', label: 'caua' },
-    { value: 'victor', label: 'victor' },
+  const months = [
+    { value: 1, label: 'Janeiro' },
+    { value: 2, label: 'Fevereiro' },
+    { value: 3, label: 'Março' },
+    { value: 4, label: 'Abril' },
+    { value: 5, label: 'Maio' },
+    { value: 6, label: 'Junho' },
+    { value: 7, label: 'Julho' },
+    { value: 8, label: 'Agosto' },
+    { value: 9, label: 'Setembro' },
+    { value: 10, label: 'Outubro' },
+    { value: 11, label: 'Novembro' },
+    { value: 12, label: 'Dezembro' },
+  ]
+
+  const years = [
+    { value: 2020, label: 2020 },
+    { value: 2021, label: 2021 },
   ]
   return (
     <Container>
       <ContentHeader title="Listas" lineColor="#FFF">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button
+          type="button"
+          className="tag-filter tag-filter-recurrent">
+          Recorrentes
+        </button>
+
+        <button
+          type="button"
+          className="tag-filter tag-filter-eventual">
+          Eventuais
+        </button>
+      </Filters>
 
       <Content>
         <HistoryFinanceCard
