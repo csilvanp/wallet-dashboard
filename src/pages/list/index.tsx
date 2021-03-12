95e129563b1c5a093d43e5a244d144b2dfa50b82
Cauã Silva <csilvanp@gmail.com>
2021-03-12 20:49:44 -0300

    format currency and date values

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -4,6 +4,8 @@ import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import gains from '../../repositories/gains';
 import expenses from '../../repositories/expenses';
+import formatCurrency from '../../utils/formatCurrency';
+import formatDate from '../../utils/formatDate';
 import { Container, Content, Filters } from './styles';
 
 interface IRouteParams {
@@ -64,14 +66,14 @@ const List: React.FC<IRouteParams> = ({ match }) => {
       return {
         id: String(Math.random() * data.length),
         description: item.description,
-        amountFormatted: item.amount,
+        amountFormatted: formatCurrency(Number(item.amount)),
         frequency: item.frequency,
-        dateFormatted: item.date,
+        dateFormatted: formatDate(item.date),
         tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
       }
     })
     setData(response);
-  }, [listData, data.length]);
+  }, [data.length, listData]);
 
   return (
     <Container>
