1061f57f26f5c908dc5f9088600c7cb648081873
Cauã Silva <csilvanp@gmail.com>
2021-03-15 18:25:28 -0300

    adding filter by transaction type

--- src/pages/list/styles.ts
+++ src/pages/list/styles.ts
@@ -15,8 +15,9 @@ export const Filters = styled.div`
     color: ${props => props.theme.colors.white};
     margin: 0 10px;
     transition: opacity .3s;
+    opacity: .4;
     :hover {
-      opacity: .7;
+      opacity: .8;
     }
   }
 
@@ -25,7 +26,7 @@ export const Filters = styled.div`
       display: block;
       width: 55px;
       margin: 0 auto;
-      border-bottom: 10px solid ${props => props.theme.colors.warning};      
+      border-bottom: 10px solid ${props => props.theme.colors.success};      
     }
 
   .tag-filter-eventual::after {
@@ -33,6 +34,10 @@ export const Filters = styled.div`
       display: block;
       width: 55px;
       margin: 0 auto;
-      border-bottom: 10px solid ${props => props.theme.colors.success};      
+      border-bottom: 10px solid ${props => props.theme.colors.warning};      
+    }
+
+    .tag-actived {
+      opacity: 1;
     }
 `;
