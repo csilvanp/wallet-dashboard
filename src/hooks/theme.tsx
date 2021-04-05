76bf408be2d081618c4b53517b230dfd0fe5b2bc
Cauã Silva <csilvanp@gmail.com>
2021-04-05 16:27:04 -0300

    saving themes in local storage

--- src/hooks/theme.tsx
+++ src/hooks/theme.tsx
@@ -34,13 +34,23 @@ interface ITheme {
 const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
 
 const ThemeProvider: React.FC = ({ children }) => {
-  const [theme, setTheme] = useState<ITheme>(dark);
+  const [theme, setTheme] = useState<ITheme>(() => {
+    const savedTheme = localStorage.getItem('@wallet:theme');
+
+    if (savedTheme) {
+      return JSON.parse(savedTheme);
+    } else {
+      return dark;
+    }
+  });
 
   const toggleTheme = () => {
     if (theme.title === 'dark') {
       setTheme(light);
+      localStorage.setItem('@wallet:theme', JSON.stringify(light));
     } else {
       setTheme(dark);
+      localStorage.setItem('@wallet:theme', JSON.stringify(dark));
     }
   };
 
