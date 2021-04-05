fa4e30d4c2c2dae7e610efbe87a359a4d3cdca4c
Cauã Silva <csilvanp@gmail.com>
2021-04-05 18:25:51 -0300

    added signin screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import logoImg from '../../assets/logo.svg';
+
+import {
+  Container,
+  Logo,
+  Form,
+  FormTitle
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <h1>SignIn</h1>
+    <Container>
+      <Logo>
+        <img src={logoImg} alt="Wallet" />
+        <h2>Wallet</h2>
+      </Logo>
+
+      <Form>
+        <FormTitle>Entrar</FormTitle>
+
+        <input type="text"></input>
+        <input type="text"></input>
+
+        <button type="submit">Acessar</button>
+      </Form>
+    </Container>
   )
 }
 
---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import SignIn from '../pages/SignIn';
+
+const AuthRoutes: React.FC = () => (
+  <Switch>
+    <Route component={SignIn} />
+  </Switch>
+);
+
+export default AuthRoutes;
