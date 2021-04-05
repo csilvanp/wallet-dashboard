d956ea949ca763d9af9f6fd08bb27908501601ab
Cauã Silva <csilvanp@gmail.com>
2021-04-05 19:48:25 -0300

    using media queries to disable aside in mobile

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -5,10 +5,15 @@ export const Grid = styled.div`
   grid-template-columns: 250px auto;
   grid-template-rows: 70px auto;
   grid-template-areas:
-  // AS = Aside
-  // MH = Main Header
-  // CT = Content
     'AS MH'
     'AS CT';
   height: 100vh;
+
+  @media(max-width: 600px){
+    grid-template-columns: 100%;
+    grid-template-rows: 70px auto;
+    grid-template-areas:
+      'MH'
+      'CT';
+  };
 `;
