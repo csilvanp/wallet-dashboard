c5866d11690e1b8c301681fc59d18d61c42a95ff
Cauã Silva <csilvanp@gmail.com>
2021-04-08 14:56:46 -0300

    fixing auth.routes.tsx file

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import SignIn from '../pages/signin';
+import SignIn from '../pages/sign-in';
 
 const AuthRoutes: React.FC = () => (
   <Switch>
